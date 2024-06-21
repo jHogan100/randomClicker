75c1b71398735411d97d08cb14f8dc78b266be61
Jack Hogan <141114760+jHogan100@users.noreply.github.com>
2024-06-21 15:03:16 +1000

    Update clicker.py

--- clicker.py
+++ clicker.py
@@ -1,12 +1,12 @@
 
-import pyautogui #use pip to unstall these libraries
+import pyautogui #use pip to install these libraries
 import random
 import time
 
 
 
 def randomClick():
-    interval = random.uniform(1.5, 1.6) #adjust for selected interval
+    interval = random.uniform(1.5, 1.6) #adjust for desired interval
     time.sleep(interval)  # Wait for the random interval
     pyautogui.click()
 
